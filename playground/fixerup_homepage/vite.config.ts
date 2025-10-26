0329b44436f10d09fa65f0954adc5825706dcb3f
tao <tao@fixerupus.com>
2025-10-26 12:38:39 -0700

    modify the js.css path

--- playground/fixerup_homepage/vite.config.ts
+++ playground/fixerup_homepage/vite.config.ts
@@ -4,68 +4,68 @@ import { resolve } from 'node:path'
 import AutoImport from 'unplugin-auto-import/vite'
 
 const base = process.env.BASE_PATH || '/'
-const isPreview = process.env.IS_PREVIEW  ? true : false;
+const isPreview = process.env.IS_PREVIEW ? true : false;
 // https://vite.dev/config/
 export default defineConfig({
   define: {
-   __BASE_PATH__: JSON.stringify(base),
-   __IS_PREVIEW__: JSON.stringify(isPreview)
+    __BASE_PATH__: JSON.stringify(base),
+    __IS_PREVIEW__: JSON.stringify(isPreview)
   },
   plugins: [react(),
-    AutoImport({
-      imports: [
-        {
-          'react': [
-            'React',
-            'useState',
-            'useEffect',
-            'useContext',
-            'useReducer',
-            'useCallback',
-            'useMemo',
-            'useRef',
-            'useImperativeHandle',
-            'useLayoutEffect',
-            'useDebugValue',
-            'useDeferredValue',
-            'useId',
-            'useInsertionEffect',
-            'useSyncExternalStore',
-            'useTransition',
-            'startTransition',
-            'lazy',
-            'memo',
-            'forwardRef',
-            'createContext',
-            'createElement',
-            'cloneElement',
-            'isValidElement'
-          ]
-        },
-        {
-          'react-router-dom': [
-            'useNavigate',
-            'useLocation',
-            'useParams',
-            'useSearchParams',
-            'Link',
-            'NavLink',
-            'Navigate',
-            'Outlet'
-          ]
-        },
-        // React i18n
-        {
-          'react-i18next': [
-            'useTranslation',
-            'Trans'
-          ]
-        }
-      ],
-      dts: true,
-    }),
+  AutoImport({
+    imports: [
+      {
+        'react': [
+          'React',
+          'useState',
+          'useEffect',
+          'useContext',
+          'useReducer',
+          'useCallback',
+          'useMemo',
+          'useRef',
+          'useImperativeHandle',
+          'useLayoutEffect',
+          'useDebugValue',
+          'useDeferredValue',
+          'useId',
+          'useInsertionEffect',
+          'useSyncExternalStore',
+          'useTransition',
+          'startTransition',
+          'lazy',
+          'memo',
+          'forwardRef',
+          'createContext',
+          'createElement',
+          'cloneElement',
+          'isValidElement'
+        ]
+      },
+      {
+        'react-router-dom': [
+          'useNavigate',
+          'useLocation',
+          'useParams',
+          'useSearchParams',
+          'Link',
+          'NavLink',
+          'Navigate',
+          'Outlet'
+        ]
+      },
+      // React i18n
+      {
+        'react-i18next': [
+          'useTranslation',
+          'Trans'
+        ]
+      }
+    ],
+    dts: true,
+  }),
   ],
-  base,
+  base: './',
   build: {
     sourcemap: true,
     outDir: 'out',
